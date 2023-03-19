06fd5ef5a52f6aae0036c26242863d9356dedff2
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-19 22:25:41 +0100

    cors middleware added

--- api/app.ts
+++ api/app.ts
@@ -1,13 +1,26 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+const { PrismaClient } = require("@prisma/client");
 
 dotenv.config();
 
+const prisma = new PrismaClient();
+
 const app: Express = express();
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type', 'Authorization', 'authorization'],
+    'exposedHeaders': ['sessionId'],
+    'origin': [process.env.UI_URL || 'http://localhost:3000'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'credentials': false,
+    'preflightContinue': false
+  }));
 export const port = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('TypeScript Server');
+app.get("/", async (req: Request, res: Response) => {
+  const posts = await prisma.Developer.findMany();
+  res.json(posts);
 });
 
 export default app;
