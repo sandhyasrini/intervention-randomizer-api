3f19794d9846df9b94471c1bf36beb61af94a08a
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-21 09:24:37 +0100

    modifications to data

--- beyond_play.sql
+++ beyond_play.sql
@@ -20,7 +20,7 @@ CREATE TABLE developers (
 
 
 COPY developers (id, name, email, role, status, team) FROM stdin(Delimiter ',');
-2, Harry Potter,harry@chosenone.com,Backend,Full Time,Team G
+2,Harry Potter,harry@chosenone.com,Backend,Full Time,Team G
 1,Albus Dumbledore,hogwarts.headmaster@gryffindor.com,Fullstack,Not available,Team G
 3,Tom Riddle,thedarklord@horcrux.com,Frontend,Contractor,Team S
 4,Luna Lovegood,blibbering_humdinger@dumbledorearmy.com,Fullstack,Full Time,Team R
